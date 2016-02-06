07a2dbcd0efc4e58db494ff64cb9a4de17120f50
Daisuke Hashimoto <galapagosit@gmail.com>
2016-02-06 19:52:36 +0900

    tmp

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from flask import Flask, session
-app = Flask(__name__)
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+from flask import Flask, current_app
+import flask
 
-@app.route("/")
-def hello():
-    if 'key' in session:
-        print(session['key'])
-    session['key'] = 'val'
-    return "Hello World!"
+app_1 = Flask('1')
+app_2 = Flask('2')
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app_1.app_context():
+        print(current_app)
+        with app_2.app_context():
+            print(current_app)
+            import pprint
+            pprint.pprint(flask._app_ctx_stack)
+        print(current_app)
 
